d5c7cd95cb3a329e29a133ba205cc701bd1c283a
Denes Nagy <dock3r@gmail.com>
2024-03-06 16:48:35 +0100

    add migrations

---
+++ Bookstore.Data/Migrations/20240306151827_Initial.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookstore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "bookstore");
+
+            migrationBuilder.CreateTable(
+                name: "Author",
+                schema: "bookstore",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Book",
+                schema: "bookstore",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PublicationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Book_Author_AuthorId",
+                        column: x => x.AuthorId,
+                        principalSchema: "bookstore",
+                        principalTable: "Author",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_AuthorId",
+                schema: "bookstore",
+                table: "Book",
+                column: "AuthorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book",
+                schema: "bookstore");
+
+            migrationBuilder.DropTable(
+                name: "Author",
+                schema: "bookstore");
+        }
+    }
+}
--- Bookstore.Models/Bookstroe.Author.cs
+++ Bookstore.Models/Bookstroe.Author.cs
@@ -3,5 +3,6 @@ namespace Bookstore.Models;
 public class Author
 {
     public Guid Id { get; set; }
+    public int Birth { get; set; }
     public List<Book> Books { get; set; }
 }
